cd54e34491bbc540cb7f63829beec9a8bbdc522d
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-11 22:50:06 +0900

    feat: 사용자 등록 엔드포인트 추가

---
+++ auth-service/src/main/java/com/ljs/smg/auth/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.ljs.smg.auth;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody @Valid AuthenticationRequest request) {
+        authenticationService.register(request);
+        
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
+    }
+}
