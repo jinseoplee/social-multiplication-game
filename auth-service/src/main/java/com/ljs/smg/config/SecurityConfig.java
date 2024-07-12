8f29e9f4e015035f33cb53b69338b40e8967243e
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-12 14:30:33 +0900

    feat: Spring Security 설정 추가

--- auth-service/src/main/java/com/ljs/smg/config/SecurityConfig.java
+++ auth-service/src/main/java/com/ljs/smg/config/SecurityConfig.java
@@ -1,20 +1,37 @@
 package com.ljs.smg.config;
 
+import com.ljs.smg.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final UserRepository userRepository;
+
     private static final String[] WHITE_LIST_URL = {
             "/api/v1/auth/**"
     };
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUserId(username)
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 회원입니다."));
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -31,5 +48,18 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
