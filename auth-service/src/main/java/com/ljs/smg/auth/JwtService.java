cac37417b2c1953f5dd68b162d878abc886e2785
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-12 18:00:06 +0900

    feat: JWT 검증 기능 추가

--- auth-service/src/main/java/com/ljs/smg/auth/JwtService.java
+++ auth-service/src/main/java/com/ljs/smg/auth/JwtService.java
@@ -1,9 +1,13 @@
 package com.ljs.smg.auth;
 
+import com.ljs.smg.user.UserRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -12,9 +16,13 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
+@RequiredArgsConstructor
 @Service
 public class JwtService {
+
+    private final UserRepository userRepository;
 
     @Value("${jwt.secret-key}")
     private String secretKey;
@@ -34,6 +42,43 @@ public class JwtService {
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public TokenValidationResponse isTokenValid(String token) {
+        try {
+            final String userId = extractUserId(token);
+            boolean isValid = userRepository.findByUserId(userId)
+                    .map(user -> userId.equals(user.getUserId()) && !isTokenExpired(token))
+                    .orElse(false);
+            return new TokenValidationResponse(isValid);
+        } catch (JwtException e) {
+            return new TokenValidationResponse(false);
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private String extractUserId(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaims(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
     private Key getSignInKey() {
