7e1ea8cf9a15363b33f68d825de55c02b6d30426
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-12 14:28:23 +0900

    feat: 사용자 인증 기능 구현

--- auth-service/src/main/java/com/ljs/smg/auth/AuthenticationService.java
+++ auth-service/src/main/java/com/ljs/smg/auth/AuthenticationService.java
@@ -4,6 +4,9 @@ import com.ljs.smg.exception.PasswordException;
 import com.ljs.smg.exception.UserIdAlreadyExistsException;
 import com.ljs.smg.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +18,8 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final AuthenticationMapper authenticationMapper;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @Transactional
     public void register(RegisterRequest request) {
@@ -24,6 +29,23 @@ public class AuthenticationService {
         var user = authenticationMapper.toUser(request, encryptedPassword);
 
         userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.userId(),
+                        request.password()
+                )
+        );
+
+        return userRepository.findByUserId(request.userId())
+                .map(user -> {
+                    var accessToken = jwtService.generateToken(user);
+                    return new AuthenticationResponse(accessToken);
+                })
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 회원입니다."));
     }
 
     private void validateAuthenticationRequest(RegisterRequest request) {
