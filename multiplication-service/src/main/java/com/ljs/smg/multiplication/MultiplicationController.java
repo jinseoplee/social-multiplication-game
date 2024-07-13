ccd264e37a85edff993c99ffc72d25f9306fb7dc
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-14 01:57:30 +0900

    feat: 곱셈 시도 검사 엔드포인트 추가

--- multiplication-service/src/main/java/com/ljs/smg/multiplication/MultiplicationController.java
+++ multiplication-service/src/main/java/com/ljs/smg/multiplication/MultiplicationController.java
@@ -1,10 +1,9 @@
 package com.ljs.smg.multiplication;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +15,10 @@ public class MultiplicationController {
     @GetMapping("/random")
     public ResponseEntity<MultiplicationResponse> getRandomMultiplication() {
         return ResponseEntity.ok(multiplicationService.createRandomMultiplication());
+    }
+
+    @PostMapping("/attempt")
+    public ResponseEntity<MultiplicationAttemptResponse> checkAttempt(@RequestBody @Valid MultiplicationAttemptRequest request) {
+        return ResponseEntity.ok(multiplicationService.checkAttempt(request));
     }
 }
