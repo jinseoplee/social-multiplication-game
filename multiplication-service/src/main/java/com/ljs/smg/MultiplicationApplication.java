0f2f3a37730c491391cbc0b6774f4a71e41bc0a8
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-14 00:25:28 +0900

    feat: JPA auditing 기능 활성화

--- multiplication-service/src/main/java/com/ljs/smg/MultiplicationApplication.java
+++ multiplication-service/src/main/java/com/ljs/smg/MultiplicationApplication.java
@@ -2,12 +2,14 @@ package com.ljs.smg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MultiplicationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MultiplicationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MultiplicationApplication.class, args);
+    }
 
 }
