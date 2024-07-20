14915fecfd0e852fc2bc4b57589fdbbc54d7707c
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-21 00:54:54 +0900

    feat: FeignClient 활성화

--- gamification-service/src/main/java/com/ljs/smg/GamificationApplication.java
+++ gamification-service/src/main/java/com/ljs/smg/GamificationApplication.java
@@ -2,8 +2,10 @@ package com.ljs.smg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GamificationApplication {
 
 	public static void main(String[] args) {
