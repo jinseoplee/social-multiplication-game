40d082642b2d8af344e11423fff118976f4a4219
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-23 00:58:17 +0900

    feat: main.js에 vue-router 추가

--- ui/src/main.js
+++ ui/src/main.js
@@ -7,8 +7,8 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
-// Components
 import App from "./App.vue";
+import router from "./router";
 
 const vuetify = createVuetify({
   components,
@@ -18,4 +18,4 @@ const vuetify = createVuetify({
   },
 });
 
-createApp(App).use(vuetify).mount("#app");
+createApp(App).use(router).use(vuetify).mount("#app");
