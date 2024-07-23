1ed60d507e1efd5a51383517ccfd32b44bed024f
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-23 13:51:34 +0900

    feat: 레이아웃 구성 추가
    - AppHeader 컴포넌트 추가
    - AppFooter 컴포넌트 추가

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,3 +1,41 @@
 <template>
-  <router-view />
+  <v-app>
+    <AppHeader :appTitle="appTitle" :barItems="barItems" />
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+
+    <AppFooter :footerItems="footerItems" />
+  </v-app>
 </template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+  data() {
+    return {
+      appTitle: "Social Multiplication Game",
+      barItems: [
+        { title: "Home", icon: "mdi-home", to: "/" },
+        { title: "Sign Up", icon: "mdi-account-plus", to: "/signup" },
+        { title: "Sign In", icon: "mdi-login", to: "/signin" },
+      ],
+      footerItems: [
+        {
+          title: "GitHub",
+          icon: "mdi-github",
+          href: "https://github.com/jinseoplee/social-multiplication-game",
+        },
+      ],
+    };
+  },
+};
+</script>
