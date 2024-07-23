7e91f70dbf7ad2a518541018c52cef0d4aa01b90
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-23 13:52:34 +0900

    feat: 푸터 컴포넌트 추가

---
+++ ui/src/components/AppFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-footer class="bg-grey-darken-4" app>
+    <v-row justify="center">
+      <v-col class="text-center" cols="12">
+        <v-btn
+          v-for="item in footerItems"
+          :key="item.title"
+          :icon="item.icon"
+          :href="item.href"
+          variant="text"
+          target="_blank"
+        ></v-btn>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter",
+  props: {
+    footerItems: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
