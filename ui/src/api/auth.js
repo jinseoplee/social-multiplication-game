b2ef61df26af945e51bc3bd661aab24e23847086
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-23 17:50:20 +0900

    feat: 로그인 API 호출 기능 추가

--- ui/src/api/auth.js
+++ ui/src/api/auth.js
@@ -5,4 +5,8 @@ const AUTH_SERVICE = "/api/v1/auth";
 // 사용자 등록(회원가입)
 const register = (userData) => post(`${AUTH_SERVICE}/register`, userData);
 
-export { register };
+// 사용자 인증(로그인)
+const authenticate = (credentials) =>
+  post(`${AUTH_SERVICE}/authenticate`, credentials);
+
+export { register, authenticate };
