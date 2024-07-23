242c1758b35510eb1e309e98c07f6e081fd8c3e4
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-23 13:52:09 +0900

    feat: 헤더 컴포넌트 추가

---
+++ ui/src/components/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app-bar app>
+    <v-app-bar-title
+      class="font-weight-bold"
+      @click="goToHome"
+      style="cursor: pointer"
+    >
+      {{ appTitle }}
+    </v-app-bar-title>
+
+    <template v-slot:append>
+      <v-btn
+        v-for="item in barItems"
+        :key="item.title"
+        :icon="item.icon"
+        :to="item.to"
+      ></v-btn>
+    </template>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  props: {
+    appTitle: {
+      type: String,
+      required: true,
+    },
+    barItems: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    goToHome() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
