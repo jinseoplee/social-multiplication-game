106252c86e38f23a8e72fc2feccc17157f65cec1
LEE JINSEOP <jjjp9966@gmail.com>
2024-07-25 16:29:53 +0900

    feat: API 요청에 액세스 토큰 자동 추가 기능 추가

--- ui/src/api/api.js
+++ ui/src/api/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import store from "@/store";
 
 const apiClient = axios.create({
   baseURL: process.env.VUE_APP_API_BASE_URL,
@@ -7,6 +8,20 @@ const apiClient = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// 요청 인터셉터 추가
+apiClient.interceptors.request.use(
+  (config) => {
+    // Vuex 스토어에서 토큰을 가져온다.
+    const accessToken = store.state.accessToken;
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 const get = (url, params = {}) => apiClient.get(url, { params });
 const post = (url, data) => apiClient.post(url, data);
